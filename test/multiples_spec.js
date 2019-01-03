91931fa0c07a03cb31345b9313d339d9ae24f071
Gene83 <gene83@protonmail.com>
2019-01-03 09:22:00 -1000

    4 tests passing

--- test/multiples_spec.js
+++ test/multiples_spec.js
@@ -1,35 +1,32 @@
-var multiples = require( './../multiples.js' ); // import the file we are testing
-var chai = require( 'chai' );
+var multiples = require("./../multiples.js"); // import the file we are testing
+var chai = require("chai");
 var should = chai.should();
 
-describe( 'Sum of a Multiple Function', function() {
-
-  it( 'should return the value 23 if given 10 as an argument', function() {
-    var sum = multiples.sumOfAMultiple( 10 );
-    sum.should.equal( 23 );
+describe("Sum of a Multiple Function", function() {
+  it("should return the value 23 if given 10 as an argument", function() {
+    var sum = multiples.sumOfAMultiple(10);
+    sum.should.equal(23);
   });
 
-  it( 'should return \'2318\' for n=100', function() {
-    var sum = multiples.sumOfAMultiple( 100 );
-    sum.should.equal( 2318 );
+  it("should return '2318' for n=100", function() {
+    var sum = multiples.sumOfAMultiple(100);
+    sum.should.equal(2318);
   });
 
   // when your tests pass, uncomment the next two and
   // change the expected value to your calculated result
-  /*
-  it( 'should return \'x\' for n=1000 (see multiples-spec.js)', function() {
+
+  it("should return '233168' for n=1000 (see multiples-spec.js)", function() {
     var sum = multiples.sumOfAMultiple(1000);
 
     // Change the argument given to .equal() to the correct number
-    sum.should.equal(0);
+    sum.should.equal(233168);
   });
 
-  it( 'should return \'x\' for n=10000 (see multiples-spec.js)', function() {
+  it("should return '23331668' for n=10000 (see multiples-spec.js)", function() {
     var sum = multiples.sumOfAMultiple(10000);
 
     // Change the argument given to .equal() to the correct number
-    sum.should.equal(0);
+    sum.should.equal(23331668);
   });
-  */
-
 });
